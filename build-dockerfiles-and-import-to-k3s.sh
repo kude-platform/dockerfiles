88df3d6a7cb4d2d3c87e2af80db199981c095b47
timo.buechert <timo.buechert@gmail.com>
2024-10-18 11:50:04 +0200

    initial

---
+++ build-dockerfiles-and-import-to-k3s.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for dir in ./evaluation/*/
+do
+    dir=${dir%*/}
+    name=${dir##*/}
+    version=$(cat "${dir}"/version.txt)
+
+    fqi="registry.local/${name}:${version}"
+    echo "Building ${fqi}"
+
+    docker build -t "${fqi}" "${dir}"
+
+    if command -v k3s &> /dev/null
+    then
+      docker save "${fqi}" | k3s ctr images import -
+    else
+      echo "k3s not found, skipping image import of ${fqi}"
+    fi
+done
---
+++ evaluation/akka-tpch-jdk11/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#current directory in the container is /tmp/app
+
+if [ -z "$JOB_COMPLETION_INDEX" ]; then
+  echo "JOB_COMPLETION_INDEX is not set"
+  exit 1
+fi
+
+if [ -z "$JOB_NAME" ]; then
+  echo "JOB_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$SVC_NAME" ]; then
+  echo "SVC_NAME is not set"
+  exit 1
+fi
+
+if [ -z "$GIT_URL" ]; then
+  echo "GIT_URL is not set"
+  exit 1
+fi
+
+git clone "$GIT_URL" ./source
+
+if [ "$UNZIP_DATA" = true ]; then
+  unzip -o ./data/TPCH.zip -d ./source/data
+fi
+
+cd ./source
+
+if [ "$OFFLINE_MODE" = true ]; then
+  mvn -o install
+else
+  mvn install
+fi
+
+cp ./target/app.jar ./app.jar
+
+if [ "$JOB_COMPLETION_INDEX" -eq 0 ]; then
+  java -jar ./app.jar master -h "$JOB_NAME-0.$SVC_NAME"
+else
+  java -jar ./app.jar worker -mh "$JOB_NAME-0.$SVC_NAME" -h "$JOB_NAME-$JOB_COMPLETION_INDEX.$SVC_NAME"
+fi
