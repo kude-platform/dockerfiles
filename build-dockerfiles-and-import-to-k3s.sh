6221ee45514ab82dbd7256ef761da7fa9b1521cb
timo.buechert <timo.buechert@gmail.com>
2024-12-31 12:22:24 +0100

    disable docker build

--- build-dockerfiles-and-import-to-k3s.sh
+++ build-dockerfiles-and-import-to-k3s.sh
@@ -15,9 +15,9 @@ do
       continue
     fi
 
-    sudo k3s ctr image list -q | grep registry.local/${name} | xargs sudo k3s ctr image del
+    ##sudo k3s ctr image list -q | grep registry.local/${name} | xargs sudo k3s ctr image del
 
-    docker build --no-cache -t "${fqi}" "${dir}"
+    ##docker build --no-cache -t "${fqi}" "${dir}"
     #docker build -t "${fqi}" "${dir}"
 
     if command -v k3s &> /dev/null
