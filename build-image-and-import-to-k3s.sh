65ff3842ec511ba37db02ee3209b436538bc007f
timo.buechert <timo.buechert@gmail.com>
2025-01-16 09:23:11 +0100

    add new script

---
+++ build-image-and-import-to-k3s.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# the first argument is the directory to build
+dir=$1
+name=${dir##*/}
+version=$(cat "${dir}"/version.txt)
+
+fqi="registry.local/${name}:${version}"
+echo "Building ${fqi}"
+
+if k3s ctr image list -q | grep -q "${fqi}"
+then
+    echo "Image ${fqi} already exists, skipping"
+    continue
+fi
+
+#sudo k3s ctr image list -q | grep registry.local/${name} | xargs sudo k3s ctr image del
+#sudo k3s ctr image list -q | grep registry.local/${name} | xargs sudo docker rmi
+
+docker build --no-cache -t "${fqi}" "${dir}"
+#docker build -t "${fqi}" "${dir}"
+
+if command -v k3s &> /dev/null
+then
+    docker save "${fqi}" | k3s ctr images import -
+else
+    echo "k3s not found, skipping image import of ${fqi}"
+fi
