2aadface4ac67ba909bb96ed087d74989fbd468d
timo.buechert <timo.buechert@gmail.com>
2025-01-17 11:37:55 +0100

    bump

--- images/akka-tpch-jdk11/entrypoint.sh
+++ images/akka-tpch-jdk11/entrypoint.sh
@@ -162,6 +162,7 @@ done
 if [ $mavenExitCode -ne 0 ]; then
   echo "Maven build failed"
   publishEvents "MVN_BUILD_FAILED" "ERROR"
+  publishLogs
   exit 1
 fi
 
@@ -187,8 +188,7 @@ if [ -n "$EVALUATION_SERVICE_ALL_PODS_READY_TO_RUN_ENDPOINT" ]; then
   echo "Waiting until all pods are ready..."
   
   while true; do
-    milliseconds_to_sleep_until_next_full_second=$((1000 - $(date +%s%3N) % 1000))
-    sleep 0.$milliseconds_to_sleep_until_next_full_second
+    sleep 1
 
     response=$(curl -s "$EVALUATION_SERVICE_ALL_PODS_READY_TO_RUN_ENDPOINT/$EVALUATION_ID")
     echo "Response from evaluation service: $response"
