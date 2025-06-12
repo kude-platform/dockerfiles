fb68a45af080b3a5878a724653dee7df0d2e3198
buechert <timo.buechert@gmail.com>
2025-06-12 14:18:32 +0200

    - add api for data-manager to stop update mechanism - update akka image to use the current maven version

--- images/data-manager/data-manager.go
+++ images/data-manager/data-manager.go
@@ -21,6 +21,10 @@ type Dataset struct {
 
 var datasets []Dataset
 
+var ticker *time.Ticker
+
+var updateInterval int
+
 func updateAvailableDatasets() {
 	resp, err := http.Get("http://" + os.Getenv("EVALUATION_SERVICE_HOST") + ":" + os.Getenv(
 		"EVALUATION_SERVICE_PORT") + "/api/data/datasets")
@@ -168,7 +172,7 @@ func containsDataset(d []Dataset, name string) bool {
 }
 
 func main() {
-	updateInterval := 30
+	updateInterval = 30
 	i, err := strconv.Atoi(os.Getenv("UPDATE_INTERVAL"))
 	if err == nil {
 		updateInterval = i
@@ -186,17 +190,68 @@ func main() {
 	}
 
 	go func() {
-		for range time.Tick(time.Second * time.Duration(updateInterval)) {
-			update()
-		}
+	    resp, err := http.Get("http://" + os.Getenv("EVALUATION_SERVICE_HOST") + ":" + os.Getenv(
+    		"EVALUATION_SERVICE_PORT") + "/api/evaluation/anyEvaluationRunning")
+
+    	if err != nil {
+    		log.Printf("Failed to get info whether any evaluation is running from evaluation service: %s", err)
+    		return
+    	}
+
+    	responseBody, err := io.ReadAll(resp.Body)
+
+    	if err != nil {
+    		log.Printf("Failed to read response body from evaluation service: %s", err)
+    		return
+    	}
+
+        boolValue, err := strconv.ParseBool(string(responseBody))
+
+        if !boolValue{
+                startTicker()
+                return
+        }
+        log.Println("Did not start update mechanism because evaluations are currently running.")
 	}()
 
 	update()
 
+	http.HandleFunc("/disableUpdateMechanism", disableUpdateMechanism)
+
+	http.HandleFunc("/enableUpdateMechanism", enableUpdateMechanism)
+
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("SERVER_PORT"), nil))
+}
+
+func startTicker(){
+    log.Println("Start update mechanism.")
+    ticker = time.NewTicker(time.Second * time.Duration(updateInterval))
+    for ; true; <-ticker.C {
+        update()
+    }
+}
+
+func stopTicker(){
+    if ticker != nil {
+        ticker.Stop()
+        log.Println("Stop update mechanism.")
+        ticker = nil
+    } else {
+        log.Println("Update mechanism not running, nothing to stop")
+    }
 }
 
 func update() {
 	updateAvailableDatasets()
 	downloadDatasets()
+}
+
+func disableUpdateMechanism(_ http.ResponseWriter, req *http.Request) {
+	stopTicker()
+}
+
+func enableUpdateMechanism(_ http.ResponseWriter, req *http.Request) {
+	go func() {
+        startTicker()
+    }()
 }
